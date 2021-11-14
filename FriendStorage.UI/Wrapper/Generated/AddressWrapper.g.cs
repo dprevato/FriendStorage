f80a3f027fcffa9c55d009d54d958806909c8142
dprevato <dprevato@gmail.com>
2021-11-14 15:02:51 +0100

    END OF COURSE - T4 template OK

--- FriendStorage.UI/Wrapper/Generated/AddressWrapper.g.cs
+++ FriendStorage.UI/Wrapper/Generated/AddressWrapper.g.cs
@@ -1,66 +1,49 @@
-
  /* Class AddressWrapper
-	* 
-	* ATTENZIONE:
-	* Questo file è stato generato automaticamente. Qualsiasi modifica apportata ad esso verrebbe riscritta alla successiva 
-	* rigenerazione del codice.
-	* Daniele Prevato, © 2022
-	*/
+    * 
+    * ATTENZIONE:
+    * Questo file è stato generato automaticamente. Qualsiasi modifica apportata ad esso verrebbe riscritta alla successiva 
+    * rigenerazione del codice.
+    * Daniele Prevato, © 2022
+    */
 
-
-	using System;
-	using System.Linq;
-	using FriendStorage.Model;
+using System;
+using System.Linq;
+using FriendStorage.Model;
 
 namespace FriendStorage.UI.Wrapper
 {
-	public partial class AddressWrapper : ModelWrapper<Address> 
-	{
-		public AddressWrapper(Address model) : base(model) { }
+  public partial class AddressWrapper : ModelWrapper<Address>
+  {
+    public AddressWrapper(Address model) : base(model)
+    {
+    }
+
+    #region property Id
+    public System.Int32 Id 	{ get => GetProperty<System.Int32>(); set => SetProperty(value); }
+    public System.Int32 IdOriginalValue => GetOriginalValue<System.Int32>(nameof(Id));
+    public bool IdIsChanged => GetIsChanged(nameof(Id));
+    #endregion property Id
 
 
-	#region property Id
-	public System.Int32 Id
-	{
-		get => GetProperty<System.Int32>();
-		set => SetProperty(value);
-	}
+    #region property City
+    public System.String City 	{ get => GetProperty<System.String>(); set => SetProperty(value); }
+    public System.String CityOriginalValue => GetOriginalValue<System.String>(nameof(City));
+    public bool CityIsChanged => GetIsChanged(nameof(City));
+    #endregion property City
 
-	public System.Int32 IdOriginalValue => GetOriginalValue<System.Int32>(nameof(Id));
-	public bool IdIsChanged => GetIsChanged(nameof(Id));
-	#endregion property Id
 
-	#region property City
-	public System.String City
-	{
-		get => GetProperty<System.String>();
-		set => SetProperty(value);
-	}
+    #region property Street
+    public System.String Street 	{ get => GetProperty<System.String>(); set => SetProperty(value); }
+    public System.String StreetOriginalValue => GetOriginalValue<System.String>(nameof(Street));
+    public bool StreetIsChanged => GetIsChanged(nameof(Street));
+    #endregion property Street
 
-	public System.String CityOriginalValue => GetOriginalValue<System.String>(nameof(City));
-	public bool CityIsChanged => GetIsChanged(nameof(City));
-	#endregion property City
 
-	#region property Street
-	public System.String Street
-	{
-		get => GetProperty<System.String>();
-		set => SetProperty(value);
-	}
+    #region property StreetNumber
+    public System.String StreetNumber 	{ get => GetProperty<System.String>(); set => SetProperty(value); }
+    public System.String StreetNumberOriginalValue => GetOriginalValue<System.String>(nameof(StreetNumber));
+    public bool StreetNumberIsChanged => GetIsChanged(nameof(StreetNumber));
+    #endregion property StreetNumber
 
-	public System.String StreetOriginalValue => GetOriginalValue<System.String>(nameof(Street));
-	public bool StreetIsChanged => GetIsChanged(nameof(Street));
-	#endregion property Street
-
-	#region property StreetNumber
-	public System.String StreetNumber
-	{
-		get => GetProperty<System.String>();
-		set => SetProperty(value);
-	}
-
-	public System.String StreetNumberOriginalValue => GetOriginalValue<System.String>(nameof(StreetNumber));
-	public bool StreetNumberIsChanged => GetIsChanged(nameof(StreetNumber));
-	#endregion property StreetNumber
-	}
+  }
 }
--- FriendStorage.UI/Wrapper/Generated/FriendGroupWrapper.g.cs
+++ FriendStorage.UI/Wrapper/Generated/FriendGroupWrapper.g.cs
@@ -1,43 +1,35 @@
  /* Class FriendGroupWrapper
-	* 
-	* ATTENZIONE:
-	* Questo file è stato generato automaticamente. Qualsiasi modifica apportata ad esso verrebbe riscritta alla successiva 
-	* rigenerazione del codice.
-	* Daniele Prevato, © 2022
-	*/
+    * 
+    * ATTENZIONE:
+    * Questo file è stato generato automaticamente. Qualsiasi modifica apportata ad esso verrebbe riscritta alla successiva 
+    * rigenerazione del codice.
+    * Daniele Prevato, © 2022
+    */
 
-
-	using System;
-	using System.Linq;
-	using FriendStorage.Model;
+using System;
+using System.Linq;
+using FriendStorage.Model;
 
 namespace FriendStorage.UI.Wrapper
 {
-	public partial class FriendGroupWrapper : ModelWrapper<FriendGroup> 
-	{
-		public FriendGroupWrapper(FriendGroup model) : base(model) { }
+  public partial class FriendGroupWrapper : ModelWrapper<FriendGroup>
+  {
+    public FriendGroupWrapper(FriendGroup model) : base(model)
+    {
+    }
+
+    #region property Id
+    public System.Int32 Id 	{ get => GetProperty<System.Int32>(); set => SetProperty(value); }
+    public System.Int32 IdOriginalValue => GetOriginalValue<System.Int32>(nameof(Id));
+    public bool IdIsChanged => GetIsChanged(nameof(Id));
+    #endregion property Id
 
 
-	#region property Id
-	public System.Int32 Id
-	{
-		get => GetProperty<System.Int32>();
-		set => SetProperty(value);
-	}
+    #region property Name
+    public System.String Name 	{ get => GetProperty<System.String>(); set => SetProperty(value); }
+    public System.String NameOriginalValue => GetOriginalValue<System.String>(nameof(Name));
+    public bool NameIsChanged => GetIsChanged(nameof(Name));
+    #endregion property Name
 
-	public System.Int32 IdOriginalValue => GetOriginalValue<System.Int32>(nameof(Id));
-	public bool IdIsChanged => GetIsChanged(nameof(Id));
-	#endregion property Id
-
-	#region property Name
-	public System.String Name
-	{
-		get => GetProperty<System.String>();
-		set => SetProperty(value);
-	}
-
-	public System.String NameOriginalValue => GetOriginalValue<System.String>(nameof(Name));
-	public bool NameIsChanged => GetIsChanged(nameof(Name));
-	#endregion property Name
-	}
+  }
 }
